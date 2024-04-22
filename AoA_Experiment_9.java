e77bae186efbf158cedc76b43ec208ffe781cac3
Parthesh Purohit <97285324+Parthesh28@users.noreply.github.com>
2024-04-22 21:12:40 +0530

    Added spurious hits

--- AoA_Experiment_9.java
+++ AoA_Experiment_9.java
@@ -1,28 +1,33 @@
 import java.util.*;
 
 public class AoA_Experiment_9 {
-
-    public static final int PRIME = 101;
+    public static final int PRIME = 11;
+    public static final int RADIX = 256;
 
     public static void Rabin_karp(String text, String pattern) {
         int n = text.length();
         int m = pattern.length();
+        int spurious_hits = 0; 
         double patternHash = hash(pattern, m);
+        double texthash = hash(text, m);
+        System.out.println("The pattern hash is: "+ patternHash);
+        System.out.println("The text hash is: "+ texthash);
 
         for (int i = 0; i <= n - m; i++) {
             int j;
             double textHash = hash(text.substring(i, i + m), m); 
             boolean match = true;
-
             if (textHash == patternHash) {
                 for (j = 0; j < m; j++) {
                     if (text.charAt(i + j) != pattern.charAt(j)) {
+                        spurious_hits = spurious_hits + 1;
                         match = false;
                         break;
                     }
                 }
                 if (match) {
-                    System.out.println("Pattern found at position: " + i);
+                    System.out.println("Pattern found after : " + i + " shifts");
+                    System.out.println("No. of Spurious Hits:  " + spurious_hits);
                 }
             }
         }
@@ -31,7 +36,7 @@ public class AoA_Experiment_9 {
     public static double hash(String str, int length) {
         double hashValue = 0;
         for (int i = 0; i < length; i++) {
-            hashValue = hashValue +  str.charAt(i) * Math.pow(PRIME, i);
+            hashValue = (hashValue * RADIX +  str.charAt(i)) % PRIME;
         }
         return hashValue;
     }
@@ -44,6 +49,5 @@ public class AoA_Experiment_9 {
         String pattern = sc.nextLine();
 
         Rabin_karp(text, pattern);
-        sc.close();
     }
 }
